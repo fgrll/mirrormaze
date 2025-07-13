ed63a1ff184a30cc34b0420f102c41a6959a1552
fgrll <florian@groll.me>
2025-07-14 01:19:52 +0200

    fix: fix mode selection layout

--- src/mirrormaze/view/ModeSelectionPanel.java
+++ src/mirrormaze/view/ModeSelectionPanel.java
@@ -1,19 +1,25 @@
 package mirrormaze.view;
 
-import java.awt.GridBagLayout;
-
+import java.awt.*;
 import javax.swing.*;
 
 
 public class ModeSelectionPanel extends JPanel {
     public ModeSelectionPanel(Runnable onStandardChosen, Runnable onSurvivalChosen) {
         setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 0; gbc.gridy = 0;
         JButton standard = new JButton("Standard");
+        add(standard, gbc);
+        gbc.gridy += 2;
         JButton survival = new JButton("Survival");
+        add(survival, gbc);
+        
         standard.addActionListener(e -> onStandardChosen.run());
         survival.addActionListener(e -> onSurvivalChosen.run());
-        add(standard);
-        add(survival);
+        
     }
     
 }
