6f2f74b7e0f5dd037c848a6412475978a684f194
fgrll <florian@groll.me>
2025-07-13 03:23:58 +0200

    fix: change dfs geneartor to boolean matrix generation

--- src/mirrormaze/generator/DFSMazeGenerator.java
+++ src/mirrormaze/generator/DFSMazeGenerator.java
@@ -3,44 +3,51 @@ package mirrormaze.generator;
 import java.util.*;
 
 public class DFSMazeGenerator implements MazeGenerator {
-    private static final char WALL = '#';
-    private static final char PATH = ' ';
+    private final Random rand = new Random();
 
     @Override
     public boolean[][] generate(int cols, int rows) {
-        char[][] maze = new char[2*cols+1][2*rows+1];
-        
-        for (char[] row : maze) Arrays.fill(row, WALL);
+
+        int W = 2 * cols + 1;
+        int H = 2 * rows + 1;
+
+        boolean[][] maze = new boolean[W][H];
+        for (boolean[] row : maze)
+            Arrays.fill(row, true);
+
         boolean[][] visited = new boolean[cols][rows];
-        Random rand = new Random();
 
-        class DFS {
-            void carve(int x, int y) {
-                visited[x][y] = true;
-                int[][] dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-                Collections.shuffle(Arrays.asList(dirs), rand);
-                for (int[] d : dirs) {
-                    int nx = x + d[0], ny = y + d[1];
-                    if (nx >= 0 && nx < cols && ny >= 0 && ny < rows && !visited[nx][ny]) {
-                        maze[x*2+1 + d[0]][y*2+1 + d[1]] = PATH;
-                        maze[nx*2+1][ny*2+1] = PATH;
-                        carve(nx, ny);
-                    }
-                }
+        carve(0, 0, cols, rows, maze, visited);
+
+        maze[0][1] = false;
+        maze[W - 1][H - 1] = false;
+
+        return maze;
+    }
+
+    private void carve(
+            int cx, int cy,
+            int cols, int rows,
+            boolean[][] maze, boolean[][] visited) {
+
+        visited[cx][cy] = true;
+
+        maze[2 * cx + 1][2 * cy + 1] = false;
+
+        List<int[]> dirs = new ArrayList<>();
+        dirs.add(new int[] { 1, 0 });
+        dirs.add(new int[] { -1, 0 });
+        dirs.add(new int[] { 0, 1 });
+        dirs.add(new int[] { 0, -1 });
+        Collections.shuffle(dirs, rand);
+
+        for (int[] d : dirs) {
+            int nx = cx + d[0], ny = cy + d[1];
+            
+            if (nx >= 0 && nx < cols && ny >= 0 && ny < rows && !visited[nx][ny]) {
+                maze[2*cx+1 + d[0]][2*cy+1 + d[1]] = false;
+                carve(nx, ny, cols, rows, maze, visited);
             }
         }
-
-        maze[1][1] = PATH;
-        new DFS().carve(0, 0);
-        maze[0][1] = PATH;
-        maze[2*cols][2*rows-1] = PATH;
-
-        boolean[][] walls = new boolean[2*cols+1][2*rows+1];
-        for (int x = 0; x < walls.length; x++) {
-            for (int y = 0; y < walls[0].length; y++) {
-                walls[x][y] = (maze[x][y] == WALL);
-            }
-        }
-        return walls;
     }
 }
